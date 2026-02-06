c2564cd25f3d122aa8b579bb51b629356e8cb473
diwakar142008 <diwakar142008@gmail.com>
2026-02-06 18:23:16 +0530

    Create main.py
    Implemented functions to add, update, delete, and view user settings.

---
+++ user--setting--manager/main.py
@@ -0,0 +1,55 @@
+** start of main.py **
+
+def add_setting(settings_dict, setting_tuple):
+    key, value = setting_tuple
+    key = key.lower()
+    value = value.lower()
+
+    if key in settings_dict:
+        return f"Setting '{key}' already exists! Cannot add a new setting with this name."
+
+    settings_dict[key] = value
+    return f"Setting '{key}' added with value '{value}' successfully!"
+
+
+def update_setting(settings_dict, setting_tuple):
+    key, value = setting_tuple
+    key = key.lower()
+    value = value.lower()
+
+    if key in settings_dict:
+        settings_dict[key] = value
+        return f"Setting '{key}' updated to '{value}' successfully!"
+
+    return f"Setting '{key}' does not exist! Cannot update a non-existing setting."
+
+
+def delete_setting(settings_dict, key):
+    key = key.lower()
+
+    if key in settings_dict:
+        del settings_dict[key]
+        return f"Setting '{key}' deleted successfully!"
+
+    return "Setting not found!"
+
+
+def view_settings(settings_dict):
+    if not settings_dict:
+        return "No settings available."
+
+    result = "Current User Settings:"
+    for key, value in settings_dict.items():
+        result += f"\n{key.capitalize()}: {value}"
+
+    return result +"\n"
+
+
+# Test dictionary (required by lab)
+test_settings = {
+    'theme':'dark',
+    'notification':'enabled'
+}
+
+** end of main.py **
+
